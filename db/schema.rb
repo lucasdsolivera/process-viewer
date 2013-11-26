13cba6c88291d0a8fa0637aad647d4de9d28b970
Lucas Oliveira <lucasstos.oliveira@gmail.com>
2013-11-25 23:01:37 -0200

    incluindo modelo forum

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131114161001) do
+ActiveRecord::Schema.define(version: 20131125002319) do
+
+  create_table "forums", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "offices", force: true do |t|
     t.string   "name"
